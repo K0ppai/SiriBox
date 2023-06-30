c8411ea7d7fbe881aadff2799749810030bf0c21
koppai <psthu.koppai@gmail.com>
2023-06-30 18:25:17 +0630

    Add tests for edge cases

--- __test__/commentCount.test.js
+++ __test__/commentCount.test.js
@@ -13,3 +13,16 @@ describe('commentCounter', () => {
     expect(count).toBe(childCount);
   });
 });
+
+describe('commentCounter', () => {
+  beforeAll(() => {
+    document.body.innerHTML = '<ul id="comments"></ul><h2 class="fs-5" id="comment-title"></h2>';
+  });
+
+  test('Comments count should be 0', async () => {
+    const ul = document.getElementById('comments');
+    const childCount = ul.childElementCount;
+    const count = await commentCounter();
+    expect(count).toBe(childCount);
+  });
+});
--- __test__/showCount.test.js
+++ __test__/showCount.test.js
@@ -1,12 +1,22 @@
 import { countShows } from '../src/modules/showCount.js';
 import '../node_modules/jsdom-global/register.js';
 
-describe('commentCounter', () => {
+describe('Shows counter', () => {
   beforeAll(() => {
     document.body.innerHTML = '<ul id="lists-container" class="row row-cols-md-3 p-0 g-md-4 m-0"><li></li><li></li></ul>';
   });
 
   test('Shows count should be 2', async () => {
     expect(countShows()).toBe(2);
+  });
+});
+
+describe('Shows Counter', () => {
+  beforeAll(() => {
+    document.body.innerHTML = '<ul id="lists-container" class="row row-cols-md-3 p-0 g-md-4 m-0"></ul>';
+  });
+
+  test('Shows count should be 0', async () => {
+    expect(countShows()).toBe(0);
   });
 });
